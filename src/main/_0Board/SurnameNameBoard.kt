a1cafc59f8bccdc65b040a7e130429e0dcd5a02a
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-10-15 17:38:02 +0300

    Added task

---
+++ src/main/_0Board/SurnameNameBoard.kt
@@ -0,0 +1,7 @@
+package task
+
+import board.GameBoard
+import board.SquareBoard
+
+fun createSquareBoard(width: Int): SquareBoard = TODO()
+fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
---
+++ src/main/_3GameOfFifteen/SurnameNameGameOfFifteen.kt
@@ -0,0 +1,10 @@
+package task
+
+import game.Game
+
+fun newGameOfFifteen(): Game = TODO()
+
+/*
+Implement the Game of Fifteen (https://en.wikipedia.org/wiki/15_puzzle).
+When you finish, you can play the game by executing 'PlayGameOfFifteen' (or choosing the corresponding run configuration).
+ */
