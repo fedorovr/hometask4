c1206448473bd6dd3aed386b2f7fdba18677feb8
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-10-18 16:29:55 +0300

    Added comment for the 'moveAndMergeEqual' function

--- src/main/_2Game2048/SurnameNameGame2048Helper.kt
+++ src/main/_2Game2048/SurnameNameGame2048Helper.kt
@@ -1,6 +1,10 @@
 package task
 
 /*
+This function moves all the non-null elements to the beginning of the list (by removing nulls) and merges equal elements.
+The parameter 'double' specifies the way how to merge equal elements:
+it returns a new element that should be present in the result list instead of two merged elements.
+
 If the function double("a") returns "aa",
 then the function moveAndMergeEqual transforms the input in the following way:
   a, a, b -> aa, b
